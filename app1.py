89251ad6b33244d4d1e1f1db1e607ad105dcef45
Velaga Lakshmi Bhuvaneswari Devi <devivelaga369@gmail.com>
2025-07-29 09:34:00 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from test_case_logic import generate_test_case
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,6 @@ def index():
     return render_template("index.html", test_cases=test_cases)
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
